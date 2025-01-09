39db9d2be3cf70c63df4a4d921f1ff826b6975af
neelmyna <nithincheeta@gmail.com>
2025-01-09 10:43:10 +0530

    testing inheritance 4 and 5 programs added

---
+++ TestingInheritance4.java
@@ -0,0 +1,20 @@
+class A1 {
+   A1(){ System.out.println("A1's no arg constructor"); }
+   A1(int a){ System.out.println(“A1's constructor "+ a); }
+}
+class B1 extends A1{
+   B1(String x){ super(50); 
+	System.out.println("B1's no arg constructor"); }
+   B1(int b){  super(1000);
+	      System.out.println(“B1‘s constructor "+ b); } 
+}
+class C1 extends B1{
+   C1() {System.out.println(“C1's no arg constructor"); }
+   C1(int c){ super(100);
+		System.out.println(“C1's constructor "+ c); }
+}
+class TestingInheritance4{
+   public static void main(String args[]){
+   	C1 ca = new C1(10);	
+   }
+}
