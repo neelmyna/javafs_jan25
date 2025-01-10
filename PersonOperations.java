b5c19c56b4c8906ce62d1f0a07996091890812d7
neelmyna <nithincheeta@gmail.com>
2025-01-10 11:32:32 +0530

    person service class added

--- PersonOperations.java
+++ PersonOperations.java
@@ -12,16 +12,23 @@ public class PersonOperations {
         persons = new ArrayList<>();
     }
 
-    private void insertPerson() {
+    private Person readPersonDetails() {
         System.out.print("Enter name of the person: ");
 	    String name = scanner.next();
 	    System.out.print("Enter gender of the person: ");
 	    char gender = scanner.next().charAt(0);
-	    Person person = new Person(name, gender);
+        
+        Person person = new Person(name, gender);
+        
+        return person;
+    }
+
+    public void insertPerson() {
+	    Person person = readPersonDetails();
 	    persons.add(person);
     }
 
-    private void deletePerson() {
+    public void deletePerson() {
         if(persons.isEmpty()) {
             System.out.println("There are no persons to delete");
             return;
@@ -38,7 +45,7 @@ public class PersonOperations {
         System.out.println("Person with id " + id + " not found");
     }
 
-    private void updatePerson() {
+    public void updatePerson() {
         if(persons.isEmpty()) {
             System.out.println("There are no persons to update");
             return;
@@ -57,7 +64,7 @@ public class PersonOperations {
         System.out.println("Person with id " + id + " not found");
     }
 
-    private void searchPerson() {
+    public void searchPerson() {
         if(persons.isEmpty()) {
             System.out.println("There are no persons to seacrh");
             return;
@@ -74,7 +81,7 @@ public class PersonOperations {
         System.out.println("Person with id " + id + " not found");
     }
 
-    private void listAllPersons() {
+    public void listAllPersons() {
         if(persons.isEmpty()) {
             System.out.println("No persons to list");
             return;
@@ -84,33 +91,5 @@ public class PersonOperations {
             System.out.printf("%-3s %-15s %s \n", person.getId(), person.getName(), person.getGender());
             //System.out.println(person);
         }
-    }
-
-    private int printMenu() {
-        System.out.println("1:Insert 2:Delete 3:Update 4:Search 5:ListALl 6:Exit \t Your Choice: ");
-        int choice = scanner.nextInt();
-        switch(choice) {
-            case 1 : this.insertPerson(); break;
-            case 2 : this.deletePerson(); break;
-            case 3 : this.updatePerson(); break;
-            case 4 : this.searchPerson(); break;
-            case 5 : this.listAllPersons(); break;
-            case 6 : System.out.println("End of Program");
-            choice = -1; break;
-            default: System.out.println("Invalid choice entered");
-        }
-        return choice;
-    }
-
-    private void startApp() {
-        int choice = 0;
-        do {
-            choice = printMenu();
-        }while(choice != -1);
-    }
-
-    public static void main(String[] args) {
-        PersonOperations oprs = new PersonOperations();
-        oprs.startApp();
     }
 }
